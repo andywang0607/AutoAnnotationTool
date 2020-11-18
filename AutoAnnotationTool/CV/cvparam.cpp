fa7de8c12f113e728d563ed5e597b73364c2a5cd
YiChih Wang <andywang0607@gmail.com>
2020-11-18 22:42:46 +0800

    [feat] Add CVParam class for parameter setting
    Note:
     - This class store cv related parameter

---
+++ AutoAnnotationTool/CV/cvparam.cpp
@@ -0,0 +1,37 @@
+#include "cvparam.h"
+
+CVParam::CVParam(QObject *parent) : QObject(parent)
+  , m_iteration(4)
+  , m_epsilon(1)
+{
+
+}
+
+int CVParam::iteration() const
+{
+    return m_iteration;
+}
+
+qreal CVParam::epsilon() const
+{
+    return m_epsilon;
+}
+
+void CVParam::setIteration(int iteration)
+{
+    if (m_iteration == iteration)
+        return;
+
+    m_iteration = iteration;
+    emit iterationChanged(m_iteration);
+}
+
+void CVParam::setEpsilon(qreal epsilon)
+{
+    qWarning("Floating point comparison needs context sanity check");
+    if (qFuzzyCompare(m_epsilon, epsilon))
+        return;
+
+    m_epsilon = epsilon;
+    emit epsilonChanged(m_epsilon);
+}
